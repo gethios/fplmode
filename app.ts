eef599d3648bdb00be40553cf2c6a71a62ef83ba
Joacim Høyland Eckholdt <joacimeckholdt@gmail.com>
2021-06-20 12:45:01 +0200

    feat(parse): Adds argument parsing from commandline
    Added --list and --team <team name> as parsable arguments to list out all teams and get league standings for a given team.

--- app.ts
+++ app.ts
@@ -1,11 +1,34 @@
+import { parse } from "https://deno.land/std@0.99.0/flags/mod.ts";
+
 import { fetchAllTeamData, fetchFixtureDataForTeam } from "./fetch.ts";
-import { getAwayResultsForTeam, getHomeResultsForTeam, getTeamId } from "./helpers.ts";
+import {
+  getAwayResultsForTeam,
+  getHomeResultsForTeam,
+  getTeamId,
+} from "./helpers.ts";
 import { Team } from "./interfaces.ts";
+import { TeamListItem } from "./types.ts";
 
 const run = async () => {
-  const name = "Man Utd";
-
+  const parseArgs = parse(Deno.args);
+  
   const teams: Team[] = await fetchAllTeamData();
+  
+  if (parseArgs.list) {
+    const teamNames: TeamListItem[] = teams.map((team) => { 
+      return {name: team.name, shorthand: team.short_name}
+    });
+    console.table(teamNames);
+
+    return;
+  }
+  
+  const name = parseArgs.team;
+  if(name === undefined) {
+    console.log('Please provide a team by using "--team <team name> | <short hand> (ex. --team "West Ham" | --team WHU)');
+    return;
+  }
+
   const teamId = getTeamId(name, teams);
 
   const fixtures = await fetchFixtureDataForTeam(teamId);
@@ -20,28 +43,28 @@ const run = async () => {
                           ${name}`);
   console.table({
     Home: [
-      { 
-        win: homeWin, 
-        draw: homeDraw, 
-        loss: homeLoss, 
-        points: homePoints 
-      }
+      {
+        win: homeWin,
+        draw: homeDraw,
+        loss: homeLoss,
+        points: homePoints,
+      },
     ],
     Away: [
-      { 
-        win: awayWin, 
-        draw: awayDraw, 
-        loss: awayLoss, 
-        points: awayPoints 
-      }
+      {
+        win: awayWin,
+        draw: awayDraw,
+        loss: awayLoss,
+        points: awayPoints,
+      },
     ],
     Total: [
-      { 
-        win: homeWin + awayWin, 
-        draw: homeDraw + awayDraw, 
-        loss: homeLoss + awayLoss, 
-        points: homePoints + awayPoints
-      }
+      {
+        win: homeWin + awayWin,
+        draw: homeDraw + awayDraw,
+        loss: homeLoss + awayLoss,
+        points: homePoints + awayPoints,
+      },
     ],
   });
 };
--- types.ts
+++ types.ts
@@ -1,1 +1,3 @@
 export type FixtureResult = [number, number, number];
+
+export type TeamListItem = { name: string; shorthand: string };
